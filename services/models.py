cad932bc596c1769005b8b0242e5c32062254d00
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-04-18 10:38:35 +0300

    created MealCategory and Meal models

--- services/models.py
+++ services/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class MealCategory(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Category")
+    description = models.TextField(verbose_name="Description")
+    image = models.ImageField(upload_to="menu/", verbose_name="Image")
+
+    class Meta:
+        verbose_name = "Meal Category"
+        verbose_name_plural = "Meal Categories"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Meal(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Name")
+    description = models.TextField(verbose_name="Description")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Price")
+    image = models.ImageField(upload_to="meals/", verbose_name="Image")
+    category = models.ForeignKey(MealCategory, on_delete=models.CASCADE, related_name="meals", verbose_name="Menu")
+
+    class Meta:
+        verbose_name = "Meal"
+        verbose_name_plural = "Meals"
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
+
