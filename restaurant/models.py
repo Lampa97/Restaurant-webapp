03b3b864b5d05e6a83e77bc58e401f470a7579e8
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-04-18 10:31:39 +0300

    created Personnel and Service models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Service Name")
+    description = models.TextField(verbose_name="Service Description")
+    image = models.ImageField(upload_to="services/", verbose_name="Service Image")
+
+    class Meta:
+        verbose_name = "Service"
+        verbose_name_plural = "Services"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Personnel(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Personnel Name")
+    position = models.CharField(max_length=100, verbose_name="Personnel Position")
+    description = models.TextField(verbose_name="Personnel Description")
+    quote = models.TextField(verbose_name="Personnel Quote")
+    photo = models.ImageField(upload_to="personnel/", verbose_name="Personnel Photo")
+
+    class Meta:
+        verbose_name = "Personnel"
+        verbose_name_plural = "Personnel"
+        ordering = ["name"]
+        unique_together = ("name", "position")
+
+    def __str__(self):
+        return f"{self.name} - {self.position}"
