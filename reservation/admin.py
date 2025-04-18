e9b55f4fa6aa2041ebad181378527b9a36e2f875
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-04-18 10:58:32 +0300

    added admin.register for models

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Table, Reservation
+
+
+@admin.register(Table)
+class CategoryTable(admin.ModelAdmin):
+    list_display = ("number", "capacity", "location")
+    search_fields = ("number", "capacity", "location")
+    list_filter = ("location", "capacity",)
+
+@admin.register(Reservation)
+class CategoryReservation(admin.ModelAdmin):
+    list_display = ("table", "customer", "date", "start_time", "end_time", "total_persons")
+    search_fields = ("table", "customer", "date", "start_time", "end_time", "total_persons")
+    list_filter = ("customer", "date", "start_time", "end_time", "total_persons")
--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MealCategory, Meal
+
+
+@admin.register(MealCategory)
+class CategoryMealCategory(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+@admin.register(Meal)
+class CategoryMeal(admin.ModelAdmin):
+    list_display = ("name", "category", "price")
+    search_fields = ("name",)
+    list_filter = ("category",)
