bb15fe9084605a4b7726b94cd8d4325fc4dbf381
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-04-26 12:43:44 +0300

    changed logic of templates home and about

--- restaurant/views.py
+++ restaurant/views.py
@@ -10,12 +10,17 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['review_form'] = ReviewForm()
-        context['personnel'] = Personnel.objects.all()
+        context['services'] = Service.objects.all()
         return context
 
 
 class AboutView(TemplateView):
     template_name = "restaurant/about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['personnel'] = Personnel.objects.all()
+        return context
 
 
 class ReviewCreateView(CreateView):
