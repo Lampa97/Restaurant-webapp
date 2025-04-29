2435fd5710b789863a2e342d5fd7577e9572e0b9
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-04-29 10:34:10 +0300

    created CRUD for Meal and MealCategory

--- services/views.py
+++ services/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView, TemplateView
+from django.views.generic import CreateView, ListView, TemplateView, UpdateView, DetailView, DeleteView
 
 from .models import Meal, MealCategory
 
@@ -34,3 +34,74 @@ class DeliveryView(TemplateView):
 
 class TourView(TemplateView):
     template_name = "services/tour.html"
+
+
+class MealListView(ListView):
+    model = Meal
+    template_name = "services/admin/meal_list.html"
+    context_object_name = "meals"
+
+    def get_queryset(self):
+        return Meal.objects.filter(category=self.kwargs["pk"])
+
+
+class MealCreateView(CreateView):
+    model = Meal
+    template_name = "services/admin/meal_create.html"
+    fields = ["name", "description", "price", "category", "image"]
+    success_url = ""
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class MealUpdateView(UpdateView):
+    model = Meal
+    template_name = "services/admin/meal_update.html"
+    fields = ["name", "description", "price", "category", "image"]
+    success_url = ""
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class MealDeleteView(DeleteView):
+    model = Meal
+    template_name = "services/admin/meal_delete.html"
+    success_url = ""
+
+
+class MealCategoryListView(ListView):
+    model = MealCategory
+    template_name = "services/admin/meal_category_list.html"
+    context_object_name = "meals"
+
+
+class MealCategoryCreateView(CreateView):
+    model = MealCategory
+    template_name = "services/admin/meal_category_create.html"
+    fields = ["name", "description", "image"]
+    success_url = ""
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class MealCategoryUpdateView(UpdateView):
+    model = MealCategory
+    template_name = "services/admin/meal_category_update.html"
+    fields = ["name", "description", "image"]
+    success_url = ""
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class MealCategoryDeleteView(DeleteView):
+    model = Meal
+    template_name = "services/admin/meal_category_delete.html"
+    success_url = ""
