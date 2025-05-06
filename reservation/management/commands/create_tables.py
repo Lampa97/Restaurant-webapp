aacd04aac44ebe3c892ae139044e094f76f8c3dd
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-05-06 16:35:15 +0300

    fixed linters errors

--- reservation/management/commands/create_tables.py
+++ reservation/management/commands/create_tables.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from restaurant.models import Table
 
+
 class Command(BaseCommand):
     help = "Create tables with predefined configurations"
 
