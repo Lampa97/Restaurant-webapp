2502d57e7aaffd865449111edd3b163ebd1ee608
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-05-06 16:11:09 +0300

    fixed linter errors

--- config/settings.py
+++ config/settings.py
@@ -106,8 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 CSRF_TRUSTED_ORIGINS = [
     f"http://{os.getenv("SERVER_IP")}",
     f"https://{os.getenv("SERVER_IP")}",
-    f"http://localhost/",
-    f"https://localhost/",
+    "http://localhost/",
+    "https://localhost/",
 ]
 
 
