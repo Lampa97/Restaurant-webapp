71e239be97bf8c19d04833a56957b8ca04317bdd
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-05-07 15:19:10 +0300

    redis fix

--- config/settings.py
+++ config/settings.py
@@ -229,6 +229,6 @@ if CACHE_ENABLED:
     CACHES = {
         "default": {
             "BACKEND": "django.core.cache.backends.redis.RedisCache",
-            "LOCATION": "redis://localhost:6379/1",
+            "LOCATION": "redis://redis:6379/1",
         }
     }
